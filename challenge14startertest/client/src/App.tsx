751fe504d60bee258195a7ac5dd13f70bb03c289
morganjtroper2 <morganjtroper@gmail.com>
2024-11-21 20:38:16 -0800

    home

--- challenge14startertest/client/src/App.tsx
+++ challenge14startertest/client/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
         <Navbar />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
           </Routes>
         </main>
