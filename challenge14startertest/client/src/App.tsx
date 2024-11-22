f94daf13753478ce21ebac096ef3866bfd330d63
morganjtroper2 <morganjtroper@gmail.com>
2024-11-21 20:36:02 -0800

    app

--- challenge14startertest/client/src/App.tsx
+++ challenge14startertest/client/src/App.tsx
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Home from './pages/Home';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <div className="container">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
