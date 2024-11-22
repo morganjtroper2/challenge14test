a1bd7eb1b14ea24cca77c8d0b6b371d722fa8fd0
morganjtroper2 <morganjtroper@gmail.com>
2024-11-21 20:11:33 -0800

    vite

--- challenge14startertest/client/vite.config.ts
+++ challenge14startertest/client/vite.config.ts
@@ -1,33 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { rewriteAll } from 'vite-plugin-rewrite-all';
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      plugins: [react()],
-      server: {
-        port: 3000,
-        open: true,
-        proxy: {
-          '/api': {
-            target: 'http://localhost:3001',
-            changeOrigin: true,
-            secure: false,
-          },
-          '/auth': {
-            target: 'http://localhost:3001',
-            changeOrigin: true,
-            secure: false,
-          },
-        },
-      },
-    };
-  } else {
-    return {
-      plugins: [react()],
-      build: {
-        outDir: 'dist',
-      },
-    };
-  }
+export default defineConfig({
+  plugins: [react(), rewriteAll()],
+  build: {
+    outDir: 'dist',
+  },
 });
