a1b218058bb39800f688811cfa1e1d5720efc0a9
morganjtroper2 <morganjtroper@gmail.com>
2024-11-21 20:21:46 -0800

    update vite

--- challenge14startertest/client/vite.config.ts
+++ challenge14startertest/client/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { rewriteAll } from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), rewriteAll()],
   build: {
     outDir: 'dist',
   },
