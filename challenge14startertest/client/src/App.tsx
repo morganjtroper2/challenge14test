ddb39946b2503d4b94b3ea5719920f30490269eb
morganjtroper2 <morganjtroper@gmail.com>
2024-11-21 20:36:58 -0800

    bjhk

--- challenge14startertest/client/src/App.tsx
+++ challenge14startertest/client/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
-import Home from './pages/Home';
 
 function App() {
   return (
